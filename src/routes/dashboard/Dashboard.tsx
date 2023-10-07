6399674575965a37659427e67bcd7c61e77b257b
Your Name <you@example.com>
2023-10-07 19:53:33 -0300

    feat: add component called Dashboard

---
+++ src/routes/dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { Header } from "./components/Header";
+
+export function Dashboard() {
+    return(
+        <div className="h-screen bg-zinc-950">
+            <Header />  
+        </div>
+    )
+}
