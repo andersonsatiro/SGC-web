c26762e78db4eb355d25b6141de57f773df18a03
Your Name <you@example.com>
2023-10-07 19:48:21 -0300

    feat: create private route

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { GlobalContext } from '../context/GlobalContext';
+
+interface PrivateRouteProps {
+  children: ReactNode;
+}
+
+export const PrivateRoute = ({ children }: PrivateRouteProps) => {
+  const { isAuthenticated } = useContext(GlobalContext)
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return <>{children}</>;
+};
+
