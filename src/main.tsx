ab31d8a393079cd5d4c43a3fb096d9e3ed5aba53
Your Name <you@example.com>
2023-10-07 19:47:38 -0300

    feat: configure React Router Dom

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom/client'
 import {App} from './App.tsx'
 import { GlobalContextProvider } from './context/GlobalContext.tsx'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Dashboard } from './routes/dashboard/Dashboard.tsx'
+import { ErrorPage } from './routes/ErrorPage.tsx'
+import { PrivateRoute } from './routes/PrivateRoute.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "dashboard",
+    element: <PrivateRoute><Dashboard /></PrivateRoute>
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalContextProvider>
-      <App />
+      <RouterProvider router={router} />
     </GlobalContextProvider>
   </React.StrictMode>,
 )
