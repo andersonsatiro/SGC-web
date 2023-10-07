c084a2f99ee1e75ce47901332efa32e56533de48
Your Name <you@example.com>
2023-10-07 19:51:48 -0300

    feat: Update GlobalContext with the state of IsAuthenticated

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -2,22 +2,29 @@ import { createContext, useState } from 'react';
 
 export const GlobalContext = createContext<{
   count: number;
+  isAuthenticated: boolean;
   click: () => void;
 
 }>({
   count: 0,
   click: () => {},
+  isAuthenticated: true,
 });
 
 export function GlobalContextProvider({ children }: { children: React.ReactNode }) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+  const [isAuthenticated] = useState(true)
 
   const click = () => {
     setCount(count + 1);
   };
 
   return (
-    <GlobalContext.Provider value={{ count, click }}>
+    <GlobalContext.Provider value={{
+      count,
+      click,
+      isAuthenticated,
+    }}>
       {children}
     </GlobalContext.Provider>
   );
