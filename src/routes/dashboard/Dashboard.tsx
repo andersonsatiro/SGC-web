f042520cd93b57fd67f3c97a73991725b1f198c9
Your Name <you@example.com>
2023-10-09 22:18:46 -0300

    feat: create functionality to return to the Login route
    create functionality to return to the login route

--- src/routes/dashboard/Dashboard.tsx
+++ src/routes/dashboard/Dashboard.tsx
@@ -1,6 +1,19 @@
-import { Header } from "./components/Header";
+import { useEffect } from "react";
+import { Header } from "./components/Header"
+import Cookies from "js-cookie"
+import { useNavigate } from "react-router-dom"
 
 export function Dashboard() {
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const token = Cookies.get('token')
+        if(!token){
+            return navigate('/')
+        }
+    },)
+
     return(
         <div className="h-screen bg-zinc-950">
             <Header />  
