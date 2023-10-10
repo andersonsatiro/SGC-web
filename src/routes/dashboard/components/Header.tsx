a1d305032c7a11c53a8a69f600b64d087f296d48
Your Name <you@example.com>
2023-10-09 22:20:57 -0300

    feat: finalize Dashboard Header functionalities

--- src/routes/dashboard/components/Header.tsx
+++ src/routes/dashboard/components/Header.tsx
@@ -1,17 +1,73 @@
 import { Users2, ArrowDownAZ, BarChart4, LogOut } from 'lucide-react'
+import Cookies from 'js-cookie'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { api } from '../../../lib/axios'
+
 export function Header() {
+    const navigate = useNavigate()
+    const [showingLeaders, setShowingLeaders] = useState(false)
+    const [leaders, setLeaders] = useState([])
+
+    interface Leader {
+        id: string,
+        influence: number,
+        jobRole: string,
+        monthlyIncome: number,
+        name: string,
+        numberOfCollaborators: number
+    }
+
+    const logout = () => {
+        Cookies.remove('token')
+        return navigate('/')
+    }
+
+    const handleShowLeaders = async () => {
+        setShowingLeaders(!showingLeaders)
+
+        const token = Cookies.get('token')
+        const {data} = await api.get('/leaders', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        setLeaders(data)
+    }
+
     return(
         <header className="flex items-center justify-between px-5 h-20 border-b-solid border-b-2 border-b-zinc-500/20">
             <div className='flex items-center gap-7 text-zinc-200 font-bold'>
-                <div
-                className="flex items-center gap-3 p-3 max-w-[150px]
-                border-solid border-[1px] border-zinc-500/20 rounded-md
-                cursor-pointer hover:bg-zinc-900/70">
+                <div className='relative'>
+                    <div
+                    className="flex items-center gap-3 p-3 max-w-[150px]
+                    border-solid border-[1px] border-zinc-500/20 rounded-md
+                    cursor-pointer hover:bg-zinc-900/90"
+                    onClick={handleShowLeaders}
+                    >
 
-                    <Users2 className='h-5 w-5 text-zinc-200' strokeWidth={1} />
-                    <h2 className='text-xs'>Lideranças</h2>
-                    <ArrowDownAZ  className='h-5 w-5 text-zinc-200' strokeWidth={1}/>
-                
+                        <Users2 className='h-5 w-5 text-zinc-200' strokeWidth={1} />
+                        <h2 className='text-xs'>Lideranças</h2>
+                        <ArrowDownAZ  className='h-5 w-5 text-zinc-200' strokeWidth={1}/>
+                    
+                    </div>
+
+                    {showingLeaders ?
+                    <div
+                        className='w-full absolute mt-1 max-w-[150px] border-solid border-[1px] 
+                        bg-zinc-950 border-zinc-500/20 rounded-md cursor-pointer'
+                        >
+                        {leaders.map((leader: Leader, index) => (
+                            <div key={leader.id} className={`flex items-center justify-between p-2 
+                            ${index !== leaders.length - 1 ? 'border-b-zinc-500/20 border-b-solid border-b-[1px]' : ''}
+                            hover:bg-zinc-900/90`}>
+                               <h1 className='text-zinc-200 text-xs'>{leader.name}</h1>
+                               <p className='text-indigo-400 text-xs'>{leader.numberOfCollaborators}</p>
+                           </div>
+                        ))}
+                        
+                    </div>
+                    : '' }
                 </div>
                 <h1 className='text-base cursor-pointer hover:text-zinc-300'>Colaboradores</h1>
                 <h1 className='text-base cursor-pointer hover:text-zinc-300'>Estatísticas</h1>
@@ -20,7 +76,9 @@ export function Header() {
             <div
                 className="flex items-center gap-3 p-3 max-w-[250px]
                 border-solid border-[1px] border-zinc-500/20 rounded-md
-                cursor-pointer hover:bg-zinc-900/70">
+                cursor-pointer hover:bg-zinc-900/90"
+                onClick={logout}
+                >
 
                 <BarChart4 className='h-3.5 w-3.5 text-zinc-200' strokeWidth={1} />
                 <h2 className='text-zinc-200 text-xs font-boldwhitespace-nowrap'>
