b5515f2e44779020355f98a37061799b48be9ce1
Your Name <you@example.com>
2023-10-13 14:38:26 -0300

    feat: check if authentication Token exist

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,15 +1,17 @@
-import { ReactNode, useContext } from 'react';
+import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
-import { GlobalContext } from '../context/GlobalContext';
+import Cookies from 'js-cookie'
 
 interface PrivateRouteProps {
   children: ReactNode;
 }
 
 export const PrivateRoute = ({ children }: PrivateRouteProps) => {
-  const { isAuthenticated } = useContext(GlobalContext)
 
-  if (!isAuthenticated) {
+  const token = Cookies.get('token')
+  const tokenExist = token ? true : false
+
+  if (!tokenExist) {
     return <Navigate to="/" />;
   }
 
