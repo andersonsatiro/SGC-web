6f850cb1689fbe941a6cbd06f73dcac00939d7d0
AndersonPaivaS <andersonpaiva135@gmail.com>
2023-11-13 19:08:02 -0300

    feta: update GlobalContext

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState} from 'react'
 import Cookies from 'js-cookie'
 import { api } from '../lib/axios';
 
@@ -7,6 +7,7 @@ interface Leader {
   influence: number,
   jobRole: string,
   monthlyIncome: number,
+  averageInfluence: number,
   name: string,
   numberOfCollaborators: number
 }
@@ -20,6 +21,12 @@ interface Collaborator {
   leaderId: string
 }
 
+interface Jobs {
+  id: string,
+  name: string,
+  influence: number
+}
+
 export const GlobalContext = createContext<{
   username: string | null,
   setUsername: (newUsername: string) => void,
@@ -29,9 +36,33 @@ export const GlobalContext = createContext<{
   getData: () => void,
   leaders: Leader[],
   collaborators: Collaborator[],
+  leadersInfluence: Leader[],
   dataExists: boolean,
 
-  submitForm: () => void,
+  leaderFormIsActive: boolean,
+  setLeaderFormIsActive: (paramsLeaderFormIsActive: boolean) => void,
+
+  jobPositions: Jobs[],
+  leadershipPositions: Jobs[],
+
+  handleInput: (paramsValue: string | number, paramsType: string, paramsContent: string) => void,
+  leadersName: string | number,
+  leaderInfluenceValue: number | string,
+  colaboratorsName: string | number,
+  collaboratorSalary: number | string,
+  CollaboratorInfluenceValue: number | string,
+  leaderJobId: string,
+  setLeaderJobId: (paramsJobId: string) => void,
+  collaboratorJobId: string,
+  setCollaboratorJobId: (paramsJobId: string) => void,
+  collaboratorLeaderId: string,
+  setCollaboratorLeaderId: (paramsCollaboratorLeaderId: string) => void,
+  submitForm: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
+
+  formErrorMessage: string,
+  formErrorMessageIsActive: boolean,
+
+
 }>({
   username: "",
   setUsername: () => {},
@@ -41,8 +72,31 @@ export const GlobalContext = createContext<{
   getData: () => {},
   leaders: [],
   collaborators: [],
+  leadersInfluence: [],
   dataExists: false,
-  submitForm: () => {}
+
+  leaderFormIsActive: false,
+  setLeaderFormIsActive: () => {},
+
+  jobPositions: [],
+  leadershipPositions: [],
+
+  handleInput: () => {},
+  leadersName: "",
+  leaderInfluenceValue: 0,
+  colaboratorsName: "",
+  collaboratorSalary: 0.0,
+  CollaboratorInfluenceValue: 0,
+  leaderJobId: "",
+  setLeaderJobId: () => {},
+  collaboratorJobId: "",
+  setCollaboratorJobId: () => {},
+  collaboratorLeaderId: "",
+  setCollaboratorLeaderId: () => {},
+  submitForm: () => {},
+
+  formErrorMessage: "",
+  formErrorMessageIsActive: false,
 });
 
 export function GlobalContextProvider({ children }: { children: React.ReactNode }) {
@@ -51,7 +105,25 @@ export function GlobalContextProvider({ children }: { children: React.ReactNode 
 
   const [leaders, setLeaders] = useState<Leader[]>([])
   const [collaborators, setCollaborators] = useState<Collaborator[]>([])
+  const [leadersInfluence, setLeadersInfluence] = useState<Leader[]>([])
   const [dataExists, setDataExists] = useState(false)
+
+  const [leaderFormIsActive, setLeaderFormIsActive] = useState(false)
+
+  const [jobPositions, setJobPositions] = useState<Jobs[]>([])
+  const [leadershipPositions, setLeadershipPositions] = useState<Jobs[]>([])
+
+  const [leadersName, setLeadersName] = useState<string | number>("")
+  const [leaderInfluenceValue, setLeaderInfluenceValue] = useState<number | string>(0)
+  const [colaboratorsName, setColaboratorsName] = useState<string | number>("")
+  const [collaboratorSalary, setCollaboratorSalary] = useState<string | number>(0.0)
+  const [CollaboratorInfluenceValue, setCollaboratorInfluenceValue] = useState<number | string>(0)
+  const [leaderJobId, setLeaderJobId] = useState("")
+  const [collaboratorJobId, setCollaboratorJobId] = useState("")
+  const [collaboratorLeaderId, setCollaboratorLeaderId] = useState("")
+  
+  const [formErrorMessage, setFormErrorMessage] = useState("")
+  const [formErrorMessageIsActive, setFormErrorMessageIsActive] = useState(false)
 
   const getData = async () => {
     const token = Cookies.get('token')
@@ -68,13 +140,115 @@ export function GlobalContextProvider({ children }: { children: React.ReactNode 
       }
     }))
     setCollaborators(collaboratorsResponse.data)
+
+    const leadersByAverageInfluence = await api.get('/leaders/influence', ({
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+    }))
+    setLeadersInfluence(leadersByAverageInfluence.data)
+
+    const jobPositionsResponse = await api.get('/jobPositions', ({
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }))
+    setJobPositions(jobPositionsResponse.data)
+
+    const leadershipPositionsResponse = await api.get('/leadershipPositions', ({
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }))
+    setLeadershipPositions(leadershipPositionsResponse.data)
+
     setDataExists(true)
   }
 
-  const submitForm = () => {
-    console.log("clicou")
-    getData()
+  const handleInput = (value: string | number, type: string, content: string) => {
+    leaderFormIsActive ?
+    (type == "text" ?  setLeadersName(value): setLeaderInfluenceValue(value))
+    :
+    (type == "text" ? setColaboratorsName(value):
+    (content == "salário" ? setCollaboratorSalary(value) : setCollaboratorInfluenceValue(value))
+    )
   }
+
+  /// SUBMIT FORM 
+
+  const submitForm = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    const token = Cookies.get('token')
+
+    if(typeof leadersName === 'string' && leadersName.trim() === ""){
+      setFormErrorMessage("Insira um nome para a Liderança")
+      setFormErrorMessageIsActive(true)
+    } else if(leaderJobId.trim() === "") {
+      setFormErrorMessage("Escola o cargo da Liderança")
+      setFormErrorMessageIsActive(true)
+    } else {
+
+
+      // Verificar a sensibilidade de maiúsculo e minúsculo
+      let leadersNameLowerCase
+      if(typeof leadersName === 'string'){
+        leadersNameLowerCase = leadersName.toLowerCase()
+      }
+
+      const leaderExists = await api.get(`leader/${leadersNameLowerCase}`, ({
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }))
+
+      if(leaderExists.data){
+        console.log("O líder existe")
+      } else {
+        console.log("o líder n existe, papai. rexpeita nóis")
+      }
+
+      /*try{
+        const response = await api.post('register/leader', {
+          name: leadersName,
+          jobId: leaderJobId,
+        }, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+
+        console.log(response.data)
+      } catch(error){
+        leaderJobId == "" ? (setFormErrorMessage("Escolha um cargo para a Liderança"), setFormErrorMessageIsActive(true)) : console.log(error)
+      }*/
+    
+    }
+  }
+
+  /// SUBMIT FORM 
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFormErrorMessageIsActive(false)
+    }, 3000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [formErrorMessageIsActive])
+
+
+  useEffect(() => {
+    setLeadersName("")
+    setLeaderInfluenceValue(0)
+    setColaboratorsName("")
+    setCollaboratorSalary(0.0)
+    setCollaboratorInfluenceValue(0)
+
+    setLeaderJobId("")
+    setCollaboratorJobId("")
+    setCollaboratorLeaderId("")
+}, [leaderFormIsActive])
 
   return (
     <GlobalContext.Provider value={{
@@ -86,8 +260,32 @@ export function GlobalContextProvider({ children }: { children: React.ReactNode 
       getData,
       leaders,
       collaborators,
+      leadersInfluence,
       dataExists,
+
+      leaderFormIsActive,
+      setLeaderFormIsActive,
+
+      jobPositions,
+      leadershipPositions,
+
+      handleInput,
+      leadersName,
+      leaderInfluenceValue,
+      colaboratorsName,
+      collaboratorSalary,
+      CollaboratorInfluenceValue,
+      leaderJobId,
+      setLeaderJobId,
+      collaboratorJobId,
+      setCollaboratorJobId,
+      collaboratorLeaderId,
+      setCollaboratorLeaderId,
       submitForm,
+
+      formErrorMessage,
+      formErrorMessageIsActive,
+
     }}>
       {children}
     </GlobalContext.Provider>
